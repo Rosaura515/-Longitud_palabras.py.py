9d0ab4e118a2b23131bb6675f60b9ab188f322d1
Rosaura Ramírez Segovia <roramirez.phyc23@bootcamp.ucamp.io>
2024-08-10 02:58:07 -0600

    Add files via upload

---
+++ #Longitud_palabras.py.py
@@ -0,0 +1,16 @@
+word_str = ("4,6 u 8 letras " )
+texto = 4
+print("palabra correcta ")
+texto = 6
+print("palabra correcta")
+texto < 4
+print("Palabra incorrecta")
+texto > 8
+print("Palabra incorrecta") 
+  
+if ("letra es un  caracter "):
+   contador = (4+1),(5+1),(6+1)
+True 
+print (input("faltan n_letras"))
+False
+print_str= (input("sobran n_letras"))
